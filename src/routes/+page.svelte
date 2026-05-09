0cee1e8234879bf538dd780d186cec44c32b5b3d
二叉树树 <acofork@qq.com>
2026-05-09 18:41:49 +0800

    fix: 分隔线还原为独立元素，直接 use:fadeIn

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -165,7 +165,7 @@
 		<TimetableCard />
 	</div>
 
-	<!-- 社交媒体链接 + 分隔线 -->
+	<!-- 社交媒体链接 -->
 	<div class="flex flex-wrap gap-3 justify-center mo-stagger" use:staggerChildren>
 		{#each siteConfig.bio.links as link}
 			{@const isLocalImage = link.icon.startsWith('/')}
@@ -184,12 +184,11 @@
 				</Button>
 			</a>
 		{/each}
-		<div class="w-full flex justify-center">
-			<Separator class="max-w-xs" />
-		</div>
 	</div>
 
-	<!-- 导航按钮 + 分隔线 -->
+	<Separator class="max-w-xs mo-fade-in" use:fadeIn />
+
+	<!-- 导航按钮 -->
 	<div class="flex flex-wrap gap-3 justify-center mo-stagger" use:staggerChildren>
 		{#each siteConfig.navLinks as link}
 			{@const isExternal = link.href.startsWith('http')}
@@ -222,10 +221,9 @@
 				Cookie 设置
 			</Button>
 		</a>
-		<div class="w-full flex justify-center">
-			<Separator class="max-w-xs" />
-		</div>
 	</div>
+
+	<Separator class="max-w-xs mo-fade-in" use:fadeIn />
 
 	<SponsorBanners />
 </div>
